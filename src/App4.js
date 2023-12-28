d481a4c7bdb1caf8234868d62d2746a743d9bf79
dudrkaak <dudrkaak@naver.com>
2023-12-28 14:08:46 +0900

    1

---
+++ src/App4.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+//todo list
+function App(){
+  const [toDo, setToDo] =  useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }    
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  }
+  return (
+  <div>
+    <h1>My TO Dos({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do..." 
+        />
+        <button>Add To Do</button>
+    </form>
+    <hr />
+    <ul>
+    {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+  </div>
+);}
+
+export default App;
