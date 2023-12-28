01d2c1038b56542f3a0bab4671e6ab4ae7a54a93
dudrkaak <dudrkaak@naver.com>
2023-12-28 17:57:01 +0900

    up

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,32 +1,39 @@
 import { Link } from "react-router-dom";
+import styles from "./MovieDetail.module.css"
 
 function MovieDetail({ title, src, description, genres, url, rating }) {
   return (
-    <div>
+    <div className={styles.moviedetail}>
       <h1>
-        <a href={url} target="_blank">
+        <a href={url} target="_blank" className={styles.moviedetail__title}>
           {title}
         </a>
       </h1>
-      <img src={src} alt={title} />
-      <h4>description</h4>
-      <p /> {description}
-      <h4>Movie Rating: {rating}</h4>
-      <div>
-        <h4>genres</h4>
-        <ul>
-          {genres.map((genre, index) => (
-            <li key={index}>{genre}</li>
-          ))}
-        </ul>
+      <div className={styles.moviedetail__ti}>
+        <img src={src} alt={title} />
+        <div className={styles.moviedetail__ti__tx}>
+          <h4 className={styles.moviedetail__rating}>Movie Rating: {rating}</h4>
+          <div className={styles.moviedetail__genres}>
+            <h4>genres</h4>
+            <ul>
+              {genres.map((genre, index) => (
+                <li key={index}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
+      <h4>description</h4>
+      <p className={styles.moviedetail__description}> {description} </p>
+      
       <br />
       <hr />
       <a href={url} target="_blank">
         Go to the site&rarr;
       </a>
-      <p />
+      <p>
       <Link to="/">Go to the Movie List &rarr;</Link>
+      </p>
     </div>
   );
 }
