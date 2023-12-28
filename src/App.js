166c488de724583ca56aad1225b391c34f9f0b62
dudrkaak <dudrkaak@naver.com>
2023-12-28 14:28:07 +0900

    123

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import Detail from "./routes/Detail";
 
 function App() {
 return (
-<BrowserRouter basename={process.env.PUBLIC_URL}>
+
 <Routes>
-<Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
-<Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} /> 
+<Route path={process.env.PUBLIC_URL + "/movie/:id"} element={<Detail />} />
+<Route path={process.env.PUBLIC_URL + "/"} element={<Home />} /> 
 </Routes>
-</BrowserRouter>
 );
 }
 
