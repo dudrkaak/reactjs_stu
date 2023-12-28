c2810fa1ae92ee2a97b31eaf3d2bbd753d8ddea2
dudrkaak <dudrkaak@naver.com>
2023-12-28 14:20:30 +0900

    12

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ return (
 <BrowserRouter>
 <Routes>
 <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />} />
-<Route path="/" element={<Home />} />
+<Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} /> 
 </Routes>
 </BrowserRouter>
 );
